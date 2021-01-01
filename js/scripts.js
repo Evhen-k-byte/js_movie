b036be98e7626f3401152c56283e8861ab503014
Evhen-k-byte <gekanator2@gmail.com>
2021-01-01 17:18:27 +0000

    add first worked version of texted film

--- js/scripts.js
+++ js/scripts.js
@@ -4,42 +4,68 @@
 
    'use strict'
 
-   let titleFilm = '';
-
    const API_KEY = '9eb126d',
       OMDb_REQ = 'http://www.omdbapi.com/?apikey=',
-      G_TRANSLATER_STRT = 'https://translate.google.ru/translate_a/t?client=x&text=',
-      G_TRANSLATER_ND = '&hl=en&sl=en&tl=ru',
       DEBUG = true;
 
    let showFilm = document.getElementById('showFilm'),
-      nameFilm = '';
+      nameFilm = null,
+      films = [],
+      filmType = document.getElementById('select').value,
+      namesFilm = [];
+
+
 
    showFilm.addEventListener('click', function () {
       nameFilm = document.getElementById('name-film').value;
 
-      async function translate() {
-         const response = await fetch(`${G_TRANSLATER_STRT}${nameFilm}${G_TRANSLATER_ND}`);
-         const translated = await response.json();
-         return translated;
-      }
-
-      async function getFilmInfo() {
-         const response = await fetch(`${OMDb_REQ}${API_KEY}&t=${nameFilm}`);
-
-         console.log(response);
-         const filmInfo = await response.json();
-         return filmInfo;
-      }
-
 
       if (nameFilm) {
-         translate();
-         // .then(translated => console.log(translated));
+         films = nameFilm.split(', ');
+
+
+         films.forEach((element) => {
+            getFilmInfo(element).then(function (obj) {
+               namesFilm.push(obj);
+               console.log(namesFilm);
+               printFilms();
+            })
+         });
+
+      } else {
+         alert('Введите хотябы одно название фильма');
       }
 
-      if (DEBUG) {
-         console.log(nameFilm);
-      }
+
+
    });
-})()
+
+   async function getFilmInfo(film) {
+      const response = await fetch(`${OMDb_REQ}${API_KEY}&t=${film}&type=${filmType}`);
+      let answer = await response.json();
+      return answer;
+   }
+
+   function printFilms() {
+
+      let ul = document.getElementById('ul'),
+         li = document.createElement('li'),
+         textNode = '';
+
+      for (let i = 0; i < namesFilm.length; i++) {
+
+         if (namesFilm[i].Title != undefined) {
+            textNode = 'Название "' + namesFilm[i].Title + '" год выпуска: '
+               + namesFilm[i].Year;
+
+         } else {
+
+            textNode = 'Название "' + films[i + 1] + '" не найдено';
+         }
+         li.innerHTML = textNode;
+         ul.append(li);
+      }
+
+      namesFilm = [];
+   }
+})();
